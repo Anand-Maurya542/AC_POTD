10fff799af0079cfdc62c039aa6714e9142149e9
Anand Kumar Maurya <anandmaurya542@gmail.com>
2025-12-10 10:30:21 +0530

    Day-10, Dec-10-2025

---
+++ day_10/add_strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        if (num2 == "0")
+            return num1;
+        if (num1 == "0")
+            return num2;
+        int i = num1.size()-1, j = num2.size()-1;
+        string ans = "";
+        int carry=0;
+        while (i >= 0 || j>=0 || carry) {
+            int sum=carry;
+            
+            if(i>=0) sum+=num1[i--]-'0';
+            if(j>=0) sum+=num2[j--]-'0';
+            ans+='0'+sum%10;
+            carry=sum/10;
+
+            
+            
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
