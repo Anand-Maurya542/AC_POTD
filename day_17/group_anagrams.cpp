6c29024abc672e6fc727abfd617b51d5474b2109
Anand Kumar Maurya <anandmaurya542@gmail.com>
2025-12-18 11:19:02 +0530

    Dec-18-2025

---
+++ day_17/group_anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        for(string s:strs){
+            string key=s;
+            sort(key.begin(),key.end());
+            mp[key].push_back(s);
+        }
+        for(auto &it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
