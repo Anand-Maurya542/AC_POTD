706b29b6cbdaafcddf5de1c4d87f68e8ff5bff82
Anand Kumar Maurya <anandmaurya542@gmail.com>
2025-12-23 12:20:36 +0530

    Dec-23-2025

---
+++ day_22/best_Time.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0,profit=0;
+        int minPrice=prices[0];
+        for(int i=1;i<prices.size();i++){
+            profit=prices[i]-minPrice;
+            maxProfit=max(profit,maxProfit);
+            minPrice=min(minPrice,prices[i]);
+
+        }
+        return maxProfit;
+        
+    }
+};
